4dad513da0cb006f3b44c77221eec28de51da604
Guy Nesher <nesher.guy@gmail.com>
2019-12-14 18:24:21 +0200

    stage-1

--- src/boilerplate/scenes/main-scene.ts
+++ src/boilerplate/scenes/main-scene.ts
@@ -23,14 +23,55 @@ export class MainScene extends Phaser.Scene {
     this.astroids = this.physics.add.group();
     this.shots = this.physics.add.group();
 
+
     // Initialize Player and Astroids
+    this.player = this.physics.add.sprite(100, 450, 'spaceship');
+
+    for (let i = 0; i < 4; i++) {
+      const astroid = this.physics.add.sprite(100 * i, 100 * i, 'astroid');
+      this.astroids.add(astroid);
+    }
 
     // Initialize Keyboard
+    this.cursors = this.input.keyboard.createCursorKeys();
 
     // Add Collision
+    this.physics.add.overlap(this.player, this.astroids, () => {
+      this.scene.restart();
+    }, null, this);
+
+    this.physics.add.overlap(this.astroids, this.shots, (astroidObj, shotObj) => {
+      astroidObj.destroy();
+      shotObj.destroy();
+    });
+
+
+
   }
 
   update(): void {
     // Add keyboard control
+    if (this.cursors.left.isDown) {
+      this.player.rotation -= 0.08;
+    }
+
+    if (this.cursors.right.isDown) {
+      this.player.rotation += 0.08;
+    }
+
+    if (this.cursors.up.isDown) {
+      this.physics.velocityFromRotation(this.player.rotation, 200, this.player.body.velocity);
+    }
+
+    if (this.cursors.space.isDown && !this.isFiring) {
+      const shot = this.physics.add.sprite(this.player.x, this.player.y, 'bullet');
+      this.shots.add(shot);
+      this.physics.velocityFromRotation(this.player.rotation, 400, shot.body.velocity);
+      this.isFiring = true;
+    }
+
+    if (this.cursors.space.isUp) {
+      this.isFiring = false;
+    }
   }
 }
